5aea79780a797a41e87553a49aceb0df402ed474
Shruti Chhikara <shrutichhikara@Shrutis-Laptop.local>
2025-03-10 00:13:44 +0530

    Adding worker processes for the parallelism.

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     testDir: 'e2etests', 
     timeout: 30000,
     retries: 2,
+    workers: 4,
     use: {
         headless: true,
         baseURL: 'http://localhost:3000',
